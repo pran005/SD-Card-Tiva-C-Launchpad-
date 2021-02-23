75840c9e102c01741c4af078fa3017bfa49a389c
Pranjal Shrivastava <52209005+pran005@users.noreply.github.com>
2021-02-23 11:14:00 +0530

    Delete SD/Objects directory

--- SD/Objects/fat32.d
+++
@@ -1,7 +0,0 @@
-.\objects\fat32.o: FAT32.c
-.\objects\fat32.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\objects\fat32.o: FAT32.h
-.\objects\fat32.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\fat32.o: UART.h
-.\objects\fat32.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
-.\objects\fat32.o: SD.h
--- SD/Objects/sd.d
+++
@@ -1,7 +0,0 @@
-.\objects\sd.o: SD.c
-.\objects\sd.o: spi.h
-.\objects\sd.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\sd.o: sd.h
-.\objects\sd.o: UART.h
-.\objects\sd.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
-.\objects\sd.o: FAT32.h
