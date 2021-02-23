d9892a6afbc3dc3cfb97540dfe65898bfc6765de
Pranjal Shrivastava <52209005+pran005@users.noreply.github.com>
2021-02-23 11:52:14 +0530

    Update SD.h

--- SD/SD.h
+++ SD/SD.h
@@ -31,7 +31,7 @@
 /**************************SD commands, many of these are not used in this code***************************/
 #define GO_IDLE_STATE            0
 #define SEND_OP_COND             1
-#define SEND_IF_COND			       8
+#define SEND_IF_COND			  8
 #define SEND_CSD                 9
 #define STOP_TRANSMISSION        12
 #define SEND_STATUS              13
@@ -43,7 +43,7 @@
 #define ERASE_BLOCK_START_ADDR   32
 #define ERASE_BLOCK_END_ADDR     33
 #define ERASE_SELECTED_BLOCKS    38
-#define SD_SEND_OP_COND			   41   //ACMD
+#define SD_SEND_OP_COND			41   //ACMD
 #define APP_CMD					             55
 #define READ_OCR                 58
 #define CRC_ON_OFF               59
